c9ea1a6f781b8ceadbe6e26c8e2bb57e2ee5c99d
raheelaahmed <155970656+raheelaahmed@users.noreply.github.com>
2024-05-25 10:31:29 +0000

    add java function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
+function getPlayerNames() {
+    const player1Name = document.getElementById('p1name').value;
+    const player2Name = document.getElementById('p2name').value;
 
+    document.getElementById('player1Score').textContent = player1Name;
+    document.getElementById('player2Score').textContent = player2Name;
+
+    // You can now proceed with the game logic (assuming you have it implemented)
+}
 
