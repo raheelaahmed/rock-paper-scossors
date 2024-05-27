178184d0d0193c98c3f1478e4742ebb79042cabe
raheelaahmed <155970656+raheelaahmed@users.noreply.github.com>
2024-05-27 19:48:22 +0000

    add buttons for player 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,50 @@ function displayPlayerNames() {
     const player2Name = localStorage.getItem('player2');
 
     if (player1Name && player2Name) {
-        document.getElementById("player1Score").textContent = player1Name + " " + "Score";
-        document.getElementById("player2Score").textContent = player2Name + " " + "Score";
+        document.getElementById("player1Score").textContent = player1Name;
+        document.getElementById("player2Score").textContent = player2Name;
     } else {
         document.getElementById("player1Score").textContent = "Player1";
         document.getElementById("player2Score").textContent = "player2";
     }
 }
+let rock = document.getElementsByClassName
+    ("rock").innerHTML;
+let paper = document.getElementsByClassName
+    ("paper").innerHTML;
+let scissors = document.getElementsByClassName
+    ("scissor").innerHTML;
+let rock2 = document.getElementsByClassName
+    ("rock2").innerHTML;
+let paper2 = document.getElementsByClassName
+    ("paper2").innerHTML;
+let scissors2 = document.getElementsByClassName
+    ("scissor2").innerHTML;
+
+
+function player1Move(button) {
+    let player1Move = prompt(rock, paper, scissors);
+    const turnsElement = document.getElementById("turns");
+
+    turnsElement.innerHTML = "Player 2 Turn";
+    const resultElement = document.getElementById("result");
+
+    resultElement.innerHTML = "player 1 is winner";
+
+
+
+}
+function player2Move(button) {
+    let player2Move = prompt(rock2, paper2, scissors2);
+    const turnsElement = document.getElementById("turns");
+
+    // Assuming you want to simply replace the content:
+    turnsElement.innerHTML = "Player 1 Turn";
+
+
+
+}
+
+
+
+
