92561e4aabba5207e9d348b02569c0a9a38739ec
raheelaahmed <155970656+raheelaahmed@users.noreply.github.com>
2024-05-29 10:27:50 +0000

    add java code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,104 @@
-function getPlayerNames() {
-    const player1Name = document.getElementById('p1name').value;
-    const player2Name = document.getElementById('p2name').value;
-    localStorage.setItem('player1', player1Name);
-    localStorage.setItem('player2', player2Name);
+
+let player;
+let computerChoice;
+let playerChoice;
 
 
 
-    window.location.href = "rock-paper-scissors.html";
-}
-function displayPlayerNames() {
-    const player1Name = localStorage.getItem('player1');
-    const player2Name = localStorage.getItem('player2');
 
-    if (player1Name && player2Name) {
-        document.getElementById("player1Score").textContent = player1Name;
-        document.getElementById("player2Score").textContent = player2Name;
+
+function gameChoice() {
+    let rock = document.getElementsByClassName("rock").innerHTML;
+
+    let paper = document.getElementsByClassName("paper").innerHTML;
+    let scissors = document.getElementsByClassName("scissor").innerHTML;
+    const playerChoice = ("rock", "paper", "scissor");
+    const choice = ("Rock", "Paper", "Scissor");
+    computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        document.getElementById("computers choice").innerHTML = "Rock";
+    } else if (computerChoice === 1) {
+        document.getElementById("computers choice").innerHTML = "Paper";
     } else {
-        document.getElementById("player1Score").textContent = "Player1";
-        document.getElementById("player2Score").textContent = "player2";
+        document.getElementById("computers choice").innerHTML = "Scissor";
     }
-}
-let rock = document.getElementsByClassName
-    ("rock").innerHTML;
-let paper = document.getElementsByClassName
-    ("paper").innerHTML;
-let scissors = document.getElementsByClassName
-    ("scissor").innerHTML;
-let rock2 = document.getElementsByClassName
-    ("rock2").innerHTML;
-let paper2 = document.getElementsByClassName
-    ("paper2").innerHTML;
-let scissors2 = document.getElementsByClassName
-    ("scissor2").innerHTML;
 
 
-function player1Move(button) {
-    let player1Move = prompt(rock, paper, scissors);
-    const turnsElement = document.getElementById("turns");
-
-    turnsElement.innerHTML = "Player 2 Turn";
-    const resultElement = document.getElementById("result");
-
-    resultElement.innerHTML = "player 1 is winner";
+    if (playerChoice === "scissor" && computerChoice === 1) {
+        document.getElementById("result").innerHTML = "player wins!";
+    }
 
 
+    else if (playerChoice === "rock" && computerChoice === 2) {
+        document.getElementById("result").innerHTML = "player wins!";
+    }
 
-}
-function player2Move(button) {
-    let player2Move = prompt(rock2, paper2, scissors2);
-    const turnsElement = document.getElementById("turns");
+    else if (playerChoice === "paper" && computerChoice === 0) {
+        document.getElementById("result").innerHTML = "player wins!";
+    }
+    if (computerChoice === 2 && playerChoice === "paper") {
+        document.getElementById("result").innerHTML = "computer wins!";
+    }
+    else if (computerChoice === 1 && playerChoice === "rock") {
+        document.getElementById("result").innerHTML = "computer wins!";
+    }
+    else if (computerChoice === 0 && playerChoice === "scissor") {
+        document.getElementById("result").innerHTML = "computer wins!";
+    }
 
-    // Assuming you want to simply replace the content:
-    turnsElement.innerHTML = "Player 1 Turn";
+    else {
+        document.getElementById("result").innerHTML = "its a Tie!";
+    }
+
 
 
 
 }
+
+
+//function moveLeft() {
+
+// let moves = parseInt(document.getElementById("movesLeft").innerHTML);
+
+// moves = moves - 1;
+// if (moves === 0) {
+//    document.getElementById("movesLeft").innerHTML = "Game Over";
+// }
+// else {
+
+//   document.getElementById("movesLeft").innerHTML = moves;
+// }
+
+
+//}
+
+//if (playerChoice === rock || playerChoice === paper || playerChoice === scissors) {
+// Update score only on valid player choices
+// let score = parseInt(document.getElementById("score").innerHTML);
+//score++
+//  ;
+// document.getElementById("score").innerHTML = score;
+// console.log("Score:", score); // Informative console output
+//} 
+//function endGame() {
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
